cf0cb55a32fade3da5b99e8b192974814597ed70
Adrian Cortes <rodrigo.cortes@udgvirtual.udg.mx>
2023-04-03 15:00:41 -0600

    Add Widgets

--- lib/main.dart
+++ lib/main.dart
@@ -65,23 +65,21 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: const <Widget>[Text('Hello World')],
+        child: Container(
+          constraints: BoxConstraints.expand(
+            height:
+                Theme.of(context).textTheme.headlineMedium!.fontSize! * 1.1 +
+                    200.0,
+          ),
+          padding: const EdgeInsets.all(8.0),
+          color: Colors.blue[600],
+          alignment: Alignment.center,
+          transform: Matrix4.rotationZ(0.1),
+          child: Text('Hello World',
+              style: Theme.of(context)
+                  .textTheme
+                  .headlineMedium!
+                  .copyWith(color: Colors.white)),
         ),
       ),
       // This trailing comma makes auto-formatting nicer for build methods.
